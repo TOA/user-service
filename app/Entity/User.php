10bec6c67b81e8a04385c7fc527d456b4e94abfb
Andrew Sannes <andrew.sannes@gmail.com>
2017-04-19 22:49:14 -0500

    added user entity

---
+++ app/Entity/User.php
@@ -0,0 +1,105 @@
+<?php
+namespace App\Entity;
+
+
+class User
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var mixed
+     */
+    private $name;
+
+    /**
+     * @var mixed
+     */
+    private $email;
+
+    /**
+     * @var mixed
+     */
+    private $password;
+
+    /**
+     * User constructor.
+     * @param int   $id
+     * @param mixed $name
+     * @param mixed $email
+     * @param mixed $password
+     */
+    public function __construct($id, $name, $email, $password)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+}
