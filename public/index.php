00422fcf7de3e04e772f6ccc12e27ea7a892b577
Andrew Sannes <andrew.sannes@gmail.com>
2017-04-25 17:42:45 -0500

    added route option trying to get CORS working

--- public/index.php
+++ public/index.php
@@ -13,6 +13,10 @@ $app = new Slim\App([
     'settings' => $config
 ]);
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
